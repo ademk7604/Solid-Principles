bf3138e324fa4792a2a082761067d7a9ebf60309
adem kok <ademk7604@gmail.com>
2023-12-08 23:55:24 +0100

    Solid Principle SO

---
+++ src/liskovSubstutionPrinciple/Araba.java
@@ -0,0 +1,5 @@
+package liskovSubstutionPrinciple;
+
+public class Araba {
+
+}
---
+++ src/openClosePrinciple/Araba.java
@@ -0,0 +1,17 @@
+package openClosePrinciple;
+
+public interface Araba {
+	
+	String model = null;
+	
+	double tripKm = 0;
+	
+	double dolarPerKm =0;
+	
+	
+	public void go();
+	
+	public void stop();
+	
+
+}
