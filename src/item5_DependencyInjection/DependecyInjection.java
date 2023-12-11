4cec3e23b64795416cde03234c13e54945e83ea9
adem kok <ademk7604@gmail.com>
2023-12-11 22:54:08 +0100

    looselty coupling

--- src/item5_DependencyInjection/DependecyInjection.java
+++ src/item5_DependencyInjection/DependecyInjection.java
@@ -3,5 +3,17 @@ package item5_DependencyInjection;
 public class DependecyInjection {
 	
 	// bei Spring boot  Constructor, Setter und Field Injection fur looselty coupling
+	
+	/*					#looselty coupling#
+	 * neden interface uzerinde diger class lara ulasip obje uretiyoruz?
+	 * 1) Direk objelerimiz uzerinden cagirma yapmiyoruz
+	 * 2) interface ler uretip, o interfeace lerde ilgili objelrle alakali class lar create ediyoruz
+	 * 3) Biz bir talepte bulunacagimiz zaman direk sinifa degilde, O siniflarin turetilmis oldugu 
+	 * 4) parent dedigimiz o interfaceler uzerinden talepte bulunuyorduk
+	 * 5) Bunun sebebi? 
+	 * a) applicationumuzin dahada rahat bir sekilde genisleyebilir olmasini sagliyor
+	 * b) Degisikliklere kapali ama gelisime acik bir hale getiriyoruz
+	 * 					#looselty coupling#
+	 */
 
 }
